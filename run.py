b85700cd7c01d9029a990d6c15a9acc142d00869
Thomas Walford <20815153+twalford@users.noreply.github.com>
2019-01-21 16:29:27 +1100

    Optimized drawing. Added point colour

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 #ilerm
 #17/01/2019
-
-#TODO
-#draw points as different colour
 from tkinter import *
+import time
 
 root = Tk()
 root.title("Pixel Splines")
-root.geometry('1320x601')
+root.geometry('700x601')#1320
 
 cellsize = 10
-canvas = Canvas(root,height=603,width=1000,bg="white",borderwidth=-3)
+canvas = Canvas(root,height=603,width=700,bg="white",borderwidth=-3)
 canvas.place(x=0,y=0)
+points = []
+pixelPath = []
 
 #textbox
 txt = Text(root,height = 30, width=10)
@@ -20,26 +20,15 @@ txt.insert(END, """0,0
 32,32
 39,52
 60,60""")
-txt.place(x=600,y=26)
+txt.place(x=601,y=26)
 
 #draw grid
 for i in range(61):
 	canvas.create_line(i*cellsize,0,i*cellsize,600,tags="grid")
 	canvas.create_line(0,i*cellsize,600,i*cellsize,tags="grid")
 	
-points = []
-points.append((0,0))
-points.append((25,9))
-points.append((32,32))
-points.append((39,52))
-points.append((60,60))
-
-#Duplicate first and last points
-points.insert(0,points[0])
-points.append(points[len(points)-1])
-	
-def DrawTile(x, y):
-	canvas.create_rectangle(x*cellsize,y*cellsize,x*cellsize+cellsize,y*cellsize+cellsize,fill="black",tags="tile")
+def DrawTile(x, y, c):
+	canvas.create_rectangle(x*cellsize,y*cellsize,x*cellsize+cellsize,y*cellsize+cellsize,fill=c,tags="tile")
 	
 def SplinePoint(t): #thanks @Javidx9
 	p1 = int(t) + 1
@@ -58,39 +47,52 @@ def SplinePoint(t): #thanks @Javidx9
 	
 	tx = 0.5 * (points[p0][0] * q1 + points[p1][0] * q2 + points[p2][0] * q3 + points[p3][0] * q4)
 	ty = 0.5 * (points[p0][1] * q1 + points[p1][1] * q2 + points[p2][1] * q3 + points[p3][1] * q4)
-	return (tx,ty)
-
-def DrawPath(*args):
-	time = 0
-	end = len(points) - 3
-	jump = 0.001
-	while time <= end:
-		tile = SplinePoint(time)
-		DrawTile(int(tile[0]),int(tile[1]))
-		time += jump
+	return (int(tx),int(ty))
 
 def UpdatePoints():
 	global points
 	str = txt.get(1.0, END)
 	strlist = str.split("\n")
-	newlist = []
+	points.clear()
+	
 	for e in strlist:
 		if e is not "":
-			newlist.append(eval(e))
-	points = newlist
+			points.append(eval(e))
+			
+	#duplicate start and end points
 	points.insert(0,points[0])
 	points.append(points[len(points)-1])
+	
+def CalculatePath():
+	time = 0
+	end = len(points) - 3
+	jump = 0.002
+	pixelPath.clear()
+	while time <= end:
+		tile = SplinePoint(time)
+		if tile not in pixelPath:
+			pixelPath.append(tile)
+		
+		time += jump
 
-def OnApply():
+def DrawPixelPath():
+	for p in pixelPath:
+		DrawTile(p[0],p[1],"black")
+
+def DrawPoints():
+	for p in points:
+		DrawTile(int(p[0]),int(p[1]),"blue")
+
+def OnApply():	
 	UpdatePoints()
 	canvas.delete("tile")
-	DrawPath()
-	print(points)
+	CalculatePath()
+	DrawPixelPath()
+	DrawPoints()
 
 #button
 btn_apply = Button(root, text="Apply", command=OnApply)
-btn_apply.place(x=600,y=0)
+btn_apply.place(x=601,y=0)
 
-DrawPath()
-
+OnApply()
 root.mainloop()
